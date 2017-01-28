560acd956364258ec1df2b472c0cce81d76a8616
hamersaw <hamersaw@bushpath.com>
2017-01-28 02:05:45 -0700

    added header

---
+++ php/header.php
@@ -0,0 +1,20 @@
+<?php
+echo <<<_END
+<div id="header">
+    <div id="title">
+        <h1>Proddle: A Distributed Network Measurement Tool</h1>
+    </div>
+
+    <div id="navbar">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="index.html#Developers">Developers</a></li>
+            <li><a href="index.html">Blog</a></li>
+            <li><a href="index.html">Vantage</a></li>
+            <li><a href="index.html">Domain</a></li>
+            <li><a href="index.html">Documentation</a></li>
+        </ul>
+    </div>
+</div>
+_END;
+?>
