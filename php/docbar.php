35a665a6ec39add790bd10bfad0e44e201e00fe3
Dan Rammer <hamersaw@bushpath.com>
2017-01-29 13:02:55 -0700

    completed capnproto documentation.

--- php/docbar.php
+++ php/docbar.php
@@ -12,12 +12,7 @@ echo <<<_END
                 <li class="navdoc"><a href="documentation.html?page=architecture-yogi">Yogi</a></li>
             </ol>
         </li>
-        <li class="navdoc"><a href="documentation.html?page=technologies">Technologies</a>
-            <ol class="navdoc">
-                <li class="navdoc"><a href="documentation.html?page=technologies-rust">Rust</a></li>
-                <li class="navdoc"><a href="documentation.html?page=technologies-capnproto">Capnproto</a></li>
-            </ol>
-        </li>
+        <li class="navdoc"><a href="documentation.html?page=capnproto">Capnproto</a></li>
     </ol>
 </div>
 _END;
--- php/documentation/technologies-rust.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo <<<_END
-<div id="docpane">
-    <h2>Technologies Rust</h2>
-    <h3>Overview</h3>
-    TODO - fill out overview
-</div>
-_END;
-?>
