fe480c5dec3270f0c90f7c9ca9a5c45f97f2ec10
Dan Rammer <hamersaw@bushpath.com>
2017-02-21 19:33:31 -0700

    updated vantage to use new aggregate mongodb query for better efficiency.

--- js/proddle.js
+++ js/proddle.js
@@ -1,3 +1,12 @@
+function addHeaderColumns(table, headerColumns) {
+    var tr = table.insertRow();
+    for (var i=0; i<headerColumns.length; i++) {
+        var th = document.createElement('th');
+        th.appendChild(document.createTextNode(headerColumns[i]));
+        tr.appendChild(th);
+    }
+}
+
 function formatDate(date) {
     return date.getUTCFullYear() + 
         '.' + (date.getUTCMonth() < 9 ? '0' + (date.getUTCMonth() + 1) : (date.getUTCMonth() + 1)) +
