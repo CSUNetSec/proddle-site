5463c768642ef6e22e7f0aebd83513bd8929914d
Dan Rammer <hamersaw@bushpath.com>
2017-03-07 12:08:37 -0700

    added marker functions to common proddle.js file. beginning step to combine functionality between vantage.html and measurement.html.

--- js/proddle.js
+++ js/proddle.js
@@ -20,3 +20,65 @@ function formatDate(date) {
         ':' + (date.getUTCSeconds() < 10 ? '0' + date.getUTCSeconds() : date.getUTCSeconds()) +
         ' UTC';
 }
+
+
+function addMarker(name, ipAddress, latitude, longitude, markerInfo, markers) {
+    //check if name is already in marker info
+    if (markerInfo.has(name)) {
+        var information = markerInfo.get(name);
+
+        //check if coordinates are already in information
+        for (var [coordinates, ipAddresses] of information) {
+            if (coordinates.latitude == latitude && coordinages.longitude == longitude) {
+
+                //check if ip address is already in store
+                for (var i=0; i<ipAddresses.length; i++) {
+                    if (ipAddresses[i] == ipAddress) {
+                        return;
+                    }
+                }
+
+                ipAddresses.push(ipAddress);
+                return;
+            }
+        }
+
+        var ipAddresses = [];
+        ipAddresses.push(ipAddress);
+        information.set({'latitude':latitude,'longitude':longitude}, ipAddresses);
+    } else {
+        var ipAddresses = [];
+        ipAddresses.push(ipAddress);
+        var information = new Map();
+        information.set({'latitude':latitude,'longitude':longitude}, ipAddresses);
+        markerInfo.set(name, information);
+    }
+}
+
+function plotMarkers(markerInfo, markers) {
+    for (var [name, information] of markerInfo) {
+        for (var [coordinates, ipAddresses] of information) {
+            //create marker
+            var marker = new google.maps.Marker({
+                position: {lat: coordinates.latitude, lng: coordinates.longitude},
+                label: name.toUpperCase()[0],
+                map: map,
+                clickable: true
+            });
+
+            addListener(marker, name, coordinates, ipAddresses);
+            markers.push(marker);
+        }
+    }
+}
+
+function addListener(marker, name, coordinates, ipAddresses) {
+    google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.setContent('<div id=content>' +
+            '<strong>Name: ' + name + '</strong>' +
+            '<br>Coordinates: (' + coordinates.latitude + ', ' + coordinates.longitude + ')' +
+            '<br>Ip Address: ' + ipAddresses +
+            '</div>');
+        infoWindow.open(map, marker);
+    });
+}
